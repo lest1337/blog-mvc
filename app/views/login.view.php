5223b2bfb0cd54764fde5954f637ce496553e2dc
Lest1337 <celestin.duverney@student.csb.school>
2026-02-17 11:55:11 +0000

    feat : login view

---
+++ app/views/login.view.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>Se connecter</h1>
+    <form action="./action=login" method="post">
+        <div id="register">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Mot de passe" required>
+            <input type="submit" hidden>
+        </div>
+    </form>
+</div>
